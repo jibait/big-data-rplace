5e07286b94fc657a78e3368646d0ad0120755c74
Frédéric Hilleriteau <46034382+hilleriteauf@users.noreply.github.com>
2024-09-08 03:49:52 +0200

    Suppression warning Spark

--- data/scripts/most-placed-color-by-coordinate.py
+++ data/scripts/most-placed-color-by-coordinate.py
@@ -13,7 +13,7 @@ import math
 # Result is formatted as `x,y,color`
 
 sc = SparkContext("local[*]", "most-placed-color-by-coordinate")
-sc.setLogLevel("WARN")
+sc.setLogLevel("ERROR")
 
 if len(sys.argv) != 3:
     print("Usage: most-placed-color-by-coordinate <input-file> <output-file>")
